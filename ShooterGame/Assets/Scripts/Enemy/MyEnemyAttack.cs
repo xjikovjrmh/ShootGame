a8d08157c1ca59b2979ed322eca5edd433761c36
b24042211@njupt.edu.cn <b24042211@njupt.edu.cn>
2026-03-25 15:31:01 +0800

    0.2
    add other enemy  and animation

--- ShooterGame/Assets/Scripts/Enemy/MyEnemyAttack.cs
+++ ShooterGame/Assets/Scripts/Enemy/MyEnemyAttack.cs
@@ -11,28 +11,35 @@ public class MyEnemyAttack : MonoBehaviour
     private float timer = 0;
 
     private MyPlayerHeath myPlayerHeath;
+    private Animator enemyAnim;
+
     private void Awake()
     {
         player = GameObject.FindGameObjectWithTag("Player");
         myPlayerHeath=player.GetComponent<MyPlayerHeath>();
+        enemyAnim=GetComponent<Animator>();  //要获取enemy的animtor组件，而不是玩家的
     }
 
     private void Update()
     {
         timer += Time.deltaTime;
-        if (PlayerInRange&&timer>2.5f)//每帧执行要有冷却
+        if (!myPlayerHeath.IsPlayerDead&&PlayerInRange&&timer>0.5f)//每帧执行要有冷却 //角色死亡就不攻击
         {
             Attack();//alter +Enter 生成方法
 
         }
-    }
+        if(myPlayerHeath.IsPlayerDead)
+        {
+            //播放死亡动画
+            enemyAnim.SetTrigger("PlayerDeath"); //这是角色死亡的触发器，播放的是敌人游荡动画，不要搞混为角色动画
+        }
 
+    }
     private void Attack()
     {
         timer = 0;//给timer置零否则一直攻击玩家
         //获取玩家的血量组件(类)，调用其方法，
         myPlayerHeath.TakeDamage(10);
-
     }
 
     //出现情况：玩家进入，离开攻击范围时调用了两次，
@@ -53,6 +60,11 @@ public class MyEnemyAttack : MonoBehaviour
         {
             PlayerInRange=false;
         }
+    
+    
+    
     }
+
+//注意，要修改预制体并覆盖原来的预制体，点击场景做好的预制体 Inspector窗口中找prefeb的overrides点击apply to all
 
 }
--- ShooterGame/Assets/Scripts/Player/MyPlayerHeath.cs
+++ ShooterGame/Assets/Scripts/Player/MyPlayerHeath.cs
@@ -1,22 +1,64 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.SearchService;
 using UnityEngine;
 
+using UnityEngine.SceneManagement;
 public class MyPlayerHeath : MonoBehaviour
 {
     //
     private int PlayerOriginalHeath = 100;
     public bool IsPlayerDead = false;
 
+    private AudioSource playerAudio;
+    public AudioClip playerDeathClip;//角色死亡音效，因为要切换，所以用public对外暴露
 
+    private Animator playerAnim;
 
+    private PlayerMovement playerMovement;
+    private MyPlayerShooting playerShooting;
+    private void Awake()
+    {
+        playerAudio = GetComponent<AudioSource>();
+        Debug.Log(playerAudio == null);
+        playerAnim= GetComponent<Animator>();
+
+        playerMovement = GetComponent<PlayerMovement>();
+        playerShooting = GetComponentInChildren<MyPlayerShooting>();//因为在子物体中
+
+    }
     public void TakeDamage(int amount )//不关心玩家受伤方向
     {
+        if (IsPlayerDead)//死亡后直接跳过，不再播放音效
+            return;
+        //播放受伤
+        playerAudio.Play();
+
         PlayerOriginalHeath -= amount;
-        Debug.Log("当前玩家血量"+PlayerOriginalHeath);
+        Debug.Log(PlayerOriginalHeath);
+        if (PlayerOriginalHeath <= 0)
+            Death();
     } 
     void Death()
     {
+        IsPlayerDead = true;
+        //播放死亡音效
+        playerAudio.clip= playerDeathClip;//切换
+        playerAudio.Play();
+        //播放死亡动画
+        playerAnim.SetTrigger("Die");//这里会
+        //禁用组件 禁止移动，射击  有bug，还能通过鼠标控制旋转，但是点击后不能
+        playerMovement.enabled = false;
+        playerShooting.enabled = false;
+    }
+    //用公共方法
+    public void RestartLevel()
+    {
+        //提前在unity文件-buidl setting -Add open Scenes
+        SceneManager.LoadScene(0);//需要引用命名空间using UnityEngine.SceneManagement;
 
+        //敌人动画改变，为idle
+
+        //重启游戏
     }
 }
